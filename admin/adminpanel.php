7c386c2216912f91067b13882d214c10db5d7db4
Anish-bijukchhe <75011742+Anish-bijukchhe@users.noreply.github.com>
2022-07-13 12:32:31 +0545

    css not conected

--- admin/adminpanel.php
+++ admin/adminpanel.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <title>Document</title>
 </head>
+
 <body>
     <div class=sidebar>
         <h3>ADMIN PANEL</h3>
@@ -19,6 +21,7 @@
             </ul>
         </div>
     </div>
-    
+
 </body>
+
 </html>
--- admin/login.php
+++ admin/login.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/admincss.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../admin/css/admincss.css">
     <title>Document</title>
 </head>
 
@@ -15,30 +17,30 @@
         <h1>
             Welcome Admin
         </h1>
-       
-        <form>
-  <div class="mb-6">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email and password with anyone else.</div>
-  </div>
-  <div class="mb-6">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
 
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+        <form>
+            <div class="mb-6">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email and password with anyone else.</div>
+            </div>
+            <div class="mb-6">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </section>
     <section class="board">
-    <img src="" alt="hotel image">
-    <ul>
-        <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i>Facebook</a></li>
-        <li><a href=""></a><i class="fa fa-instagram" aria-hidden="true">Instagram</i></li>
-        <li><a href=""></a>Booking.com</li>
-        <li><a href=""></a>Expedia</li>
-        
-    </ul>
+        <img src="../images/logo.png" alt="hotel image">
+        <ul style="margin:20px;padding:30px;">
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Booking.com</a></li>
+            <li><a href="#">Expedia</a></li>
+
+        </ul>
 
     </section>
 
